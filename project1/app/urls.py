cd5188d63f5ae4848e756b6f9dfc51a8f8129f40
takashi-shinohara <www.shino@icloud.com>
2022-06-02 10:55:40 +0900

    delete

--- project1/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from . import responseAI
-
-urlpatterns = [
-    path('sample/', views.input),
-    path('sample/output', views.output),
-    path('example.com/', responseAI.dummyAI),
-]
